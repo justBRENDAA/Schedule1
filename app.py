056a43d57348da92f7243df06d91157aa50a70dd
justBRENDAA <107068477+justBRENDAA@users.noreply.github.com>
2025-04-14 11:43:17 -0500

    landing page and product page changesPlease enter the commit message for your changes. Lines starting

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/weed')
+def weed(): 
+    return render_template('weed.html')
+
+@app.route('/meth')
+def meth(): 
+    return render_template('meth.html')
+
+@app.route('/cocaine')
+def cocaine(): 
+    return render_template('cocaine.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
